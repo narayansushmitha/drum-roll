f37d06ef9df74cc02da35d6fa1a0948a1e3258e4
narayansushmitha <63325913+narayansushmitha@users.noreply.github.com>
2020-04-16 16:20:20 -0400

    Initial files

---
+++ index.js
@@ -0,0 +1,42 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var audio = new Audio(makeSound(this.innerHTML));
+    audio.play();
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  var audio = new Audio(makeSound(event.key.toLowerCase()));
+  audio.play();
+});
+
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      return 'sounds/tom-1.mp3';
+
+    case "a":
+      return 'sounds/tom-2.mp3';
+
+    case "s":
+      return 'sounds/tom-3.mp3';
+
+    case "d":
+      return 'sounds/tom-4.mp3';
+
+    case "j":
+      return 'sounds/snare.mp3';
+
+    case "k":
+      return 'sounds/crash.mp3';
+
+    case "l":
+      return 'sounds/kick-bass.mp3';
+
+    default:
+      return key;
+  }
+}
